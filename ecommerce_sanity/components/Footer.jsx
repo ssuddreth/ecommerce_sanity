4c5aa830278f340f2e455339586d8521ba118ba1
Steven Suddreth <stevensuddreth@gmail.com>
2022-12-30 17:05:36 -0500

    add social media links and create responsive css

--- ecommerce_sanity/components/Footer.jsx
+++ ecommerce_sanity/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillInstagram, AiFillFacebook, AiFillYoutube } from 'react-icons/ai';
+import { BsSpotify } from "react-icons/bs";
+import { SiApplemusic } from "react-icons/si";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2022 Fear Until Fury All rights reserverd</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a className='social-links' href='https://www.instagram.com/fearuntilfury/' target='_blank'><AiFillInstagram /></a>
+        <a className='social-links' href='https://www.facebook.com/fearuntilfury/' target='_blank'><AiFillFacebook /></a>
+        <a className='social-links' href='https://www.youtube.com/c/FearUntilFury' target='_blank'><AiFillYoutube /></a>
+        <a className='social-links' href='https://open.spotify.com/artist/2GTyFGo1MgTrbORh3Qt5HA' target='_blank'><BsSpotify /></a>
+        <a className='social-links' href='https://music.apple.com/us/artist/fear-until-fury/1220578778' target='_blank'><SiApplemusic /></a>
       </p>
     </div>
   )
